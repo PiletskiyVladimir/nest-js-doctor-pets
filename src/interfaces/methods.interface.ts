ec7b13664a8d6f82714f47dc1825a2b1fae2f01d
Пілецький Володимир <piletskiy10@gmail.com>
2022-03-28 22:11:50 +0300

    methods.interface.ts redone to Promise wrap to async func work properly

--- src/interfaces/methods.interface.ts
+++ src/interfaces/methods.interface.ts
@@ -2,31 +2,31 @@ import { ValidateError } from "errors-checker/index";
 import { TLoginResponse } from "../types";
 
 export interface IGetAllMethod<T> {
-    getAll(query: {[prop: string]: any}): Promise<T[]> | Promise<ValidateError[]>;
+    getAll(query: {[prop: string]: any}): Promise<T[] | ValidateError[]>;
 }
 
 export interface IGetByIdMethod<T> {
-    getById(id: number): Promise<T> | Promise<ValidateError[]>;
+    getById(id: number): Promise<T | ValidateError[]>;
 }
 
 export interface ICreateMethod<T> {
-    create(body: any): Promise<T> | Promise<ValidateError[]>;
+    create(body: any): Promise<T | ValidateError[]>;
 }
 
 export interface IUpdateMethod<T> {
-    update(id: number, updateBody: any): Promise<T> | Promise<ValidateError[]>;
+    update(id: number, updateBody: any): Promise<T | ValidateError[]>;
 }
 
 export interface IDeleteMethod {
-    delete(id: number): Promise<null> | Promise<ValidateError[]>;
+    delete(id: number): Promise<null | ValidateError[]>;
 }
 
 export interface ILoginMethod {
-    login(body:any): Promise<TLoginResponse> | Promise<ValidateError[]>
+    login(body:any): Promise<TLoginResponse | ValidateError[]>
 }
 
 export interface IRegistrationMethod<T> {
-    registration(body: any): Promise<T> | Promise<ValidateError[]>
+    registration(body: any): Promise<T | ValidateError[]>
 }
 
 export interface ILogoutMethod {
